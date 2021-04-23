5b4837bb345c9c77b653ebc9800a72606c2f13f7
Bruno Sautron <sautron.brunojj@gmail.com>
2021-04-23 15:32:29 -0600

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+const https = require('https')
+const { exec } = require('child_process');
+const basePath = '/home/bruno/workspace/perso'
+const { chdir } = require('process');
+const express = require('express')
+
+const promiseExec = cmd => {
+    console.log(`$ ${cmd}`)
+    return new Promise((res, rej) => {
+        exec(cmd, (err, stdout, stderr) => {
+            console.log('err:', err) /* dump variable */
+            console.log('stderr:', stderr) /* dump variable */
+            console.log('stdout:', stdout) /* dump variable */
+            console.log('')
+            if (err) return rej(err)
+            return res(stdout)
+        })
+
+    })
+}
+
+function cdToProject(projectName) {
+    return new Promise((res, rej) => {
+        try {
+            chdir(`${basePath}/${projectName}`)
+            res()
+        } catch (err) {
+            rej(err)
+        }
+    })
+}
+
+function execGitPull(remoteName, branchName) {
+    return promiseExec(`git pull ${remoteName} ${branchName}`)
+}
+function execDCBuild() {
+    return promiseExec(`docker-compose -f docker-compose.prod.yml up -d --build`)
+}
+
+const app = express()
+    .use(express.json())
+    .post('/deploy', (req, res) => {
+        cdToProject(req.body.repository.name)
+            .then(() => execGitPull('origin', 'main'))
+            .then(execDCBuild)
+            .then(() => res.send('Finish to deploy'))
+            .catch(() => res.statusCode(500).send('an error occurs'))
+    })
+app.listen(4040, () => console.log('http://localhost:4040'))
+
